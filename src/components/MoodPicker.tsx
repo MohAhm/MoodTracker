d41e83c34ecb3da958de9cfd5a643a33f8fc51fd
Mohamed Ali <a.mohamed.cali@gmail.com>
2024-03-03 22:19:43 +0100

    Refactor MoodPicker component to include pressable mood options

--- src/components/MoodPicker.tsx
+++ src/components/MoodPicker.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { theme } from '../theme';
+import { MoodOptionType } from '../types';
 
-const moodOptions = [
+const moodOptions: MoodOptionType[] = [
   { emoji: '🧑‍💻', description: 'studious' },
   { emoji: '🤔', description: 'pensive' },
   { emoji: '😊', description: 'happy' },
@@ -10,19 +12,85 @@ const moodOptions = [
 ];
 
 export const MoodPicker = () => {
+  const [selectedMode, setSelectedMode] = useState<MoodOptionType>();
+
   return (
-    <View style={styles.moodOptions}>
-      {moodOptions.map(mood => (
-        <Text key={mood.emoji}>{mood.emoji}</Text>
-      ))}
+    <View style={styles.container}>
+      <Text style={styles.heading}>How are you right now?</Text>
+      <View style={styles.moodList}>
+        {moodOptions.map(mood => (
+          <View>
+            <Pressable
+              onPress={() => setSelectedMode(mood)}
+              style={[
+                styles.moodItem,
+                selectedMode?.emoji === mood.emoji
+                  ? styles.selectedMoodItem
+                  : undefined,
+              ]}>
+              <Text key={mood.emoji}>{mood.emoji}</Text>
+            </Pressable>
+            {selectedMode?.emoji === mood.emoji && (
+              <Text style={styles.descriptionText}>{mood.description}</Text>
+            )}
+          </View>
+        ))}
+      </View>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Choose</Text>
+      </Pressable>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  moodOptions: {
+  container: {
+    borderWidth: 2,
+    borderColor: theme.colorPurple,
+    margin: 10,
+    borderRadius: 10,
+    padding: 20,
+  },
+  moodList: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingHorizontal: 20,
+  },
+  moodItem: {
+    height: 60,
+    width: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+  },
+  selectedMoodItem: {
+    borderWidth: 2,
+    borderColor: '#fff',
+    backgroundColor: theme.colorPurple,
+  },
+  descriptionText: {
+    color: theme.colorPurple,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 10,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    letterSpacing: 1,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: theme.colorPurple,
+    width: 150,
+    borderRadius: 20,
+    marginTop: 20,
+    alignSelf: 'center',
+    padding: 10,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
 });
