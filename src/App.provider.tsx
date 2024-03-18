3d193634e798de63492e99859f35a2510263dd56
Mohamed Ali <a.mohamed.cali@gmail.com>
2024-03-18 22:57:52 +0100

    Use Async Storage for persisting data

--- src/App.provider.tsx
+++ src/App.provider.tsx
@@ -1,5 +1,29 @@
-import React, { createContext, useCallback, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { createContext, useCallback, useEffect, useState } from 'react';
 import { MoodOptionType, MoodOptionWithTimestamp } from './types';
+
+type AppData = {
+  modeList: MoodOptionWithTimestamp[];
+};
+
+const dataKey = 'my-app-data';
+
+const setAppData = async (data: AppData): Promise<void> => {
+  try {
+    await AsyncStorage.setItem(dataKey, JSON.stringify(data));
+  } catch {}
+};
+
+const getAppData = async (): Promise<AppData | null> => {
+  try {
+    const result = await AsyncStorage.getItem(dataKey);
+    if (result) {
+      return JSON.parse(result);
+    }
+  } catch {}
+
+  return null;
+};
 
 type AppContextType = {
   moodList: MoodOptionWithTimestamp[];
@@ -19,13 +43,30 @@ export const AppProvider = ({ children }: AppProviderProps) => {
   const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
 
   const handleSelectMood = useCallback((selectedMood: MoodOptionType) => {
-    setMoodList(current => [
-      ...current,
-      {
-        mood: selectedMood,
-        timestamp: Date.now(),
-      },
-    ]);
+    setMoodList(current => {
+      const newModeList = [
+        ...current,
+        {
+          mood: selectedMood,
+          timestamp: Date.now(),
+        },
+      ];
+
+      setAppData({ modeList: newModeList });
+
+      return newModeList;
+    });
+  }, []);
+
+  useEffect(() => {
+    const fetchAppData = async () => {
+      const data = await getAppData();
+      if (data) {
+        setMoodList(data.modeList);
+      }
+    };
+
+    fetchAppData();
   }, []);
 
   return (
