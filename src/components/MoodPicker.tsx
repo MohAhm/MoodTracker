44e769d93f363d7c3eda352aa5a4a3413d3c5315
Mohamed Ali <a.mohamed.cali@gmail.com>
2024-03-25 22:52:38 +0100

    Add background image to Home screen

--- src/components/MoodPicker.tsx
+++ src/components/MoodPicker.tsx
@@ -76,6 +76,7 @@ const styles = StyleSheet.create({
     margin: 10,
     borderRadius: 10,
     padding: 20,
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
   },
   moodList: {
     flexDirection: 'row',
@@ -105,6 +106,7 @@ const styles = StyleSheet.create({
     letterSpacing: 1,
     textAlign: 'center',
     marginBottom: 20,
+    color: theme.colorWhite,
   },
   button: {
     backgroundColor: theme.colorPurple,
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ImageBackground, StyleSheet } from 'react-native';
 import { useAppContext } from '../App.provider';
 import { MoodPicker } from '../components/MoodPicker';
+
+const imageUrl =
+  'https://images.unsplash.com/photo-1474540412665-1cdae210ae6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1766&q=80';
 
 export const Home = () => {
   const { handleSelectMood } = useAppContext();
 
   return (
-    <View style={styles.container}>
+    <ImageBackground style={styles.container} source={{ uri: imageUrl }}>
       <MoodPicker handleSelectMood={handleSelectMood} />
-    </View>
+    </ImageBackground>
+
+    // <View style={styles.container}>
+    //   <Image source={{ uri: imageUrl }} style={{ flex: 1 }} />
+    //   <View style={[StyleSheet.absoluteFill, { justifyContent: 'center' }]}>
+    //     <MoodPicker handleSelectMood={handleSelectMood} />
+    //   </View>
+    // </View>
   );
 };
 
